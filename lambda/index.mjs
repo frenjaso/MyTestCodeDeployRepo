9304830a1f2f324cd74c0058ce56243750af9253
frenjaso <60863202+frenjaso@users.noreply.github.com>
2023-06-13 08:05:32 -0700

    Making a change for Dan

--- lambda/index.mjs
+++ lambda/index.mjs
@@ -2,7 +2,7 @@ export const handler = async(event) => {
     // TODO implement
     const response = {
         statusCode: 200,
-        body: JSON.stringify('Hello from Lambda222!'),
+        body: JSON.stringify('Hello from Lambda2223333!'),
     };
     return response;
 };
