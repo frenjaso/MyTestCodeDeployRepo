0c960f4ea9580d57bd72b0747461fd955d5f1909
frenjaso <60863202+frenjaso@users.noreply.github.com>
2023-06-13 12:06:26 -0700

    test changing lambda code

--- lambda/index.mjs
+++ lambda/index.mjs
@@ -2,7 +2,7 @@ export const handler = async(event) => {
     // TODO implement
     const response = {
         statusCode: 200,
-        body: JSON.stringify('Hello from Lambda2223333!'),
+        body: JSON.stringify('Hello from Lambda4444!'),
     };
     return response;
 };
